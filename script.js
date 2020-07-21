45bdfaa284a0354ad851107010d78c3399a38b26
vijay2249 <65058877+vijay2249@users.noreply.github.com>
2020-07-21 17:33:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var deletebtn = document.getElementsByClassName("delete");
+var items = ul.getElementsByTagName("li");
+
+for (var i = 0; i < deletebtn.length; i++){
+	deletebtn[i].addEventListener("click",removeParent,false);
+}
+
+function removeParent(evt){
+	evt.target.removeEventListener("click",removeParent,false);
+	evt.target.parentNode.remove();
+}
+
+function getEventTarget(e){
+	e = e || window.event;
+	return e.target || e.srcElement;
+}
+
+ul.onclick = function(event){
+	var target = getEventTarget(event);
+	target.classList.toggle("done");
+}
+
+function inputLength(){
+	return input.value.length;
+}
+
+function createlistElement(){
+	var btn = document.createElement("button");
+	btn.innerHTML = "Delete";
+	btn.onclick = removeParent;
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	li.innerHTML = li.innerHTML + " ";
+	li.appendChild(btn);
+	ul.appendChild(li);
+	input.value = "";
+}
+
+function addToListAfterClick(){
+	if(inputLength()>0){
+		createlistElement();
+	}
+}
+
+function addTolistAfterKeypress(event){
+	if(inputLength()>0 && event.keyCode === 13){
+		createlistElement();
+	}
+}
+
+button.addEventListener("click",addToListAfterClick);
+input.addEventListener("keypress",addTolistAfterKeypress);
